55ecc31705f581145a4301a4da8dc924f229a498
Olamilekan Adenusi <lekan.adenusi@gmail.com>
2023-09-13 15:19:28 +0100

    Update helper_functions.py
    Changed a few things, majorly in pred_and_plot, load_and_prep functions

--- extras/helper_functions.py
+++ extras/helper_functions.py
@@ -1,10 +1,12 @@
 ### We create a bunch of helpful functions throughout the course.
 ### Storing them here so they're easily accessible.
 
+import matplotlib.pyplot as plt
 import tensorflow as tf
+plt.rcParams['figure.figsize'] = (5.0, 3.6)
 
 # Create a function to import an image and resize it to be able to be used with our model
-def load_and_prep_image(filename, img_shape=224, scale=True):
+def load_and_prep_img(filename, img_size=(224, 224), scale=True, resize_method=tf.image.ResizeMethod.NEAREST_NEIGHBOR):
   """
   Reads in an image from filename, turns it into a tensor and reshapes into
   (224, 224, 3).
@@ -12,20 +14,21 @@ def load_and_prep_image(filename, img_shape=224, scale=True):
   Parameters
   ----------
   filename (str): string filename of target image
-  img_shape (int): size to resize target image to, default 224
+  img_size (tuple): size to resize target image to, default (224, 224)
   scale (bool): whether to scale pixel values to range(0, 1), default True
+  resize_method (tf.image.ResizeMethod): method to use in resizing the image
   """
   # Read in the image
-  img = tf.io.read_file(filename)
-  # Decode it into a tensor
-  img = tf.image.decode_jpeg(img)
+  img = tf.image.decode_image(tf.io.read_file(file_path), channels=3)
+
   # Resize the image
-  img = tf.image.resize(img, [img_shape, img_shape])
+  resized_img = tf.image.resize(img, size=img_size, method=resize_method)
+
   if scale:
     # Rescale the image (get all values between 0 and 1)
-    return img/255.
+    return tf.expand_dims(resized_img/255, axis=0)
   else:
-    return img
+    return tf.expand_dims(resized_img, axis=0)
 
 # Note: The following confusion matrix code is a remix of Scikit-Learn's 
 # plot_confusion_matrix function - https://scikit-learn.org/stable/modules/generated/sklearn.metrics.plot_confusion_matrix.html
@@ -110,30 +113,34 @@ def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_s
     fig.savefig("confusion_matrix.png")
   
 # Make a function to predict on images and plot them (works with multi-class)
-def pred_and_plot(model, filename, class_names):
+def pred_and_plot(model, filename, class_names, resize_method=tf.image.ResizeMethod.NEAREST_NEIGHBOR):
   """
   Imports an image located at filename, makes a prediction on it with
   a trained model and plots the image with the predicted class as the title.
   """
   # Import the target image and preprocess it
-  img = load_and_prep_image(filename)
+  img = load_and_prep_img(filename, resize_method=resize_method)
 
   # Make a prediction
-  pred = model.predict(tf.expand_dims(img, axis=0))
-
+  pred = model.predict(img, verbose=0)
+  
   # Get the predicted class
   if len(pred[0]) > 1: # check for multi-class
     pred_class = class_names[pred.argmax()] # if more than one output, take the max
+    pred_prob = tf.reduce_max(pred).squeeze()
   else:
     pred_class = class_names[int(tf.round(pred)[0][0])] # if only one output, round
+    pred_prob = pred[0][0]
+    if pred_prob <= 0.5:
+      pred_prob = 1 - pred_prob
 
-  # Plot the image and predicted class
-  plt.imshow(img)
-  plt.title(f"Prediction: {pred_class}")
-  plt.axis(False);
+  # plot image
+  plt.imshow(img.squeeze())
+  plt.title(f'prediction: {(pred_prob*100).round(1)}% {pred_class}', fontsize=10)
+  plt.axis('off');
+  
   
 import datetime
-
 def create_tensorboard_callback(dir_name, experiment_name):
   """
   Creates a TensorBoard callback instand to store log files.
@@ -145,12 +152,11 @@ def create_tensorboard_callback(dir_name, experiment_name):
     dir_name: target directory to store TensorBoard log files
     experiment_name: name of experiment directory (e.g. efficientnet_model_1)
   """
-  log_dir = dir_name + "/" + experiment_name + "/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-  tensorboard_callback = tf.keras.callbacks.TensorBoard(
-      log_dir=log_dir
-  )
+  log_dir = f'{dir_name}/{experiment_name}/{datetime.datetime.now().strftime("%Y_%m_%d-%H.%M")}'
   print(f"Saving TensorBoard log files to: {log_dir}")
-  return tensorboard_callback
+  callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir)
+
+  return callback
 
 # Plot the validation and training data separately
 import matplotlib.pyplot as plt
@@ -171,6 +177,7 @@ def plot_loss_curves(history):
   epochs = range(len(history.history['loss']))
 
   # Plot loss
+  plt.subplot(121)
   plt.plot(epochs, loss, label='training_loss')
   plt.plot(epochs, val_loss, label='val_loss')
   plt.title('Loss')
@@ -178,14 +185,14 @@ def plot_loss_curves(history):
   plt.legend()
 
   # Plot accuracy
-  plt.figure()
+  plt.subplot(122)
   plt.plot(epochs, accuracy, label='training_accuracy')
   plt.plot(epochs, val_accuracy, label='val_accuracy')
   plt.title('Accuracy')
   plt.xlabel('Epochs')
   plt.legend();
 
-def compare_historys(original_history, new_history, initial_epochs=5):
+def compare_histories(original_history, new_history, initial_epochs=5):
     """
     Compares two TensorFlow model History objects.
     
@@ -262,7 +269,8 @@ def walk_through_dir(dir_path):
       name of each subdirectory
   """
   for dirpath, dirnames, filenames in os.walk(dir_path):
-    print(f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'.")
+    if len(filenames) > 0:
+      print(f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'.")
     
 # Function to evaluate: accuracy, precision, recall, f1-score
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
